fde8ebeeb7ee5d886d4ace56e6d4304904624036
qeuif <166937424+qeuif@users.noreply.github.com>
2025-02-16 02:58:32 +0800

    Added logic to play a single round

--- javascript.js
+++ javascript.js
@@ -28,4 +28,57 @@ function getHumanChoice(){
     }
 }
 
-getHumanChoice()
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    // console.log(humanChoice);
+    // console.log(computerChoice);
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("It\'s a tie! Both played Rock");
+                break;
+            case "paper":
+                console.log("You lose! Paper beats Rock");
+                computerScore += 1;
+                break;
+            case "scissors":
+                console.log("You win! Rock beats Scissors");
+                humanScore += 1;
+        }
+    } else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "paper":
+                console.log("It\'s a tie! Both played Paper");
+                break;
+            case "scissors":
+                console.log("You lose! Scissors beats Paper");
+                computerScore += 1;
+                break;
+            case "rock":
+                console.log("You win! Paper beats Rock");
+                humanScore += 1;
+        }
+    } else { // humanChoice is scissors
+        switch (computerChoice) {
+            case "scissors":
+                console.log("It\'s a tie! Both played Scissors");
+                break;
+            case "rock":
+                console.log("You lose! Rock beats Scissors");
+                computerScore += 1;
+                break;
+            case "paper":
+                console.log("You win! Scissors beats Paper");
+                humanScore += 1;
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore);
+console.log(computerScore);
