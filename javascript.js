d8337a941313a667c53c6669efbb2279d860601b
qeuif <166937424+qeuif@users.noreply.github.com>
2025-02-16 02:32:07 +0800

    Added logic to get computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+// console.log("hello world!");
+
+function getComputerChoice() {
+    let choice = Math.random();
+    // console.log(choice);
+    if (choice < 0.33) {
+        return "rock";
+    } else if (choice < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+// console.log(getComputerChoice());
